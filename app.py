a834a60caf80b617527c0ec38ccfd6e7f05957dd
Jialu Zhang <jialuzhang@foxmail.com>
2024-12-29 12:40:04 +0800

    modify table head & title & footer

--- app.py
+++ app.py
@@ -5,15 +5,18 @@ import duckdb
 conn = duckdb.connect()
 
 
-st.title('使用 SQL 搜索 Excel 文件')
+st.set_page_config(page_title='核新云', layout='centered')
 
-hasHeader = st.checkbox('有表头', value=True)
-headerNames = '' if hasHeader else st.text_input('表头（英文逗号分隔，如：a,b,c）')
+
+st.title('搜索DCS信息构建PSA图形')
+
+hasHeader = st.checkbox('有表头（无表头则默认列名为0，1，2，3...）', value=True)
     
 uploaded_file = st.file_uploader('选择文件', ['xls', 'xlsx', 'xlsm', 'xlsb', 'odf', 'ods', 'odt'])
 
 if uploaded_file is not None:
-    dataframes = pd.read_excel(uploaded_file, sheet_name=None, header= 0 if hasHeader else None, names=headerNames.split(',') if headerNames else None)
+    dataframes = pd.read_excel(uploaded_file, sheet_name=None, header= 'a' if hasHeader else None)
+    # dataframes = pd.read_excel(uploaded_file, sheet_name=None, header= 0 if hasHeader else None, names=headerNames.split(',') if headerNames else None)
     tabs = st.tabs(dataframes.keys())
     
     for index, (name, df) in enumerate(dataframes.items()):
@@ -24,3 +27,34 @@ if uploaded_file is not None:
 query = st.text_area('查询')
 if query:
     st.write(conn.execute(query).df())
+
+
+footer="""<style>
+a:link , a:visited{
+color: blue;
+background-color: transparent;
+text-decoration: underline;
+}
+
+a:hover,  a:active {
+color: red;
+background-color: transparent;
+text-decoration: underline;
+}
+
+.footer {
+position: fixed;
+left: 0;
+bottom: 0;
+width: 100%;
+background-color: white;
+color: black;
+text-align: center;
+}
+</style>
+<div class="footer">
+<p>&copy 核新云科技（南京）有限公司</p>
+</div>
+"""
+
+st.markdown(footer,unsafe_allow_html=True)
